77f9986dc95419a59a6924769a929ed73015da10
--replace-all <terrencejung928@gmail.com>
2022-07-11 01:54:10 -0700

    commit tree b940e773ec6eb7ebc4e6d3d978009b4b4c19b328

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,27 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    new_baked_good = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    new_baked_good.to_json
+  end
+
+  patch '/bakeries/:id' do
+    update_bakery = Bakery.all.find(params[:id])
+    update_bakery.update(
+      name: params[:name]
+    )
+    update_bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    delete_baked_good = BakedGood.all.find(params[:id])
+    delete_baked_good.destroy
+    delete_baked_good.to_json
+  end
+
 end
